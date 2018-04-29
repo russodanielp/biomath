298bc022972ef5ccd386f37e041746006cb5928a
Daniel P. Russo <russodanielp@gmail.com>
2018-04-29 12:00:09 -0400

    working on calculating nullspace in python

--- PeripheralModelFiles/python_scripts/calc_null.py
+++ PeripheralModelFiles/python_scripts/calc_null.py
@@ -2,12 +2,11 @@ from sympy import Matrix, Symbol, sympify
 import pandas as pd
 
 
-S = pd.read_csv('../text_files/S_matrix_python_27x62.csv')
+S = pd.read_csv('../text_files/S_matrix_python_27x62_for_python.csv', index_col=0)
+
+init_params = pd.read_csv('../text_files/initial_params.csv', index_col=0, header=None)
 
 def  convert(x):
-    if type(x) == str:
-        return sympify(x)
-    else:
         return sympify(x)
 
 
@@ -15,24 +14,35 @@ p_1 = 'piecewise(0,or(eq(MAb_dose_amount,0),ge(floor((Time_Sim-MAb_dose_start)/M
 p_2 = 'piecewise(0,or(eq(Statin_dose_amount,0),ge(floor((Time_Sim-Statin_dose_start)/Statin_dose_interval),Statin_dose_cycles)),Statin_dose_amount/Statin_MW/convert_nmol_to_mmol/Statin_dose_duration,and(ge(Time_Sim,Statin_dose_start+ge(Time_Sim,Statin_dose_start)*floor((Time_Sim-Statin_dose_start)/Statin_dose_interval)*Statin_dose_interval),lt(Time_Sim,Statin_dose_start+Statin_dose_duration+ge(Time_Sim,Statin_dose_start)*floor((Time_Sim-Statin_dose_start)/Statin_dose_interval)*Statin_dose_interval)),0)+piecewise(0,or(eq(Statin_doseX_amount,0),ge(floor((Time_Sim-Statin_doseX_start)/Statin_doseX_interval),Statin_doseX_cycles)),Statin_doseX_amount/Statin_MW/convert_nmol_to_mmol/Statin_doseX_duration,and(ge(Time_Sim,Statin_doseX_start+ge(Time_Sim,Statin_doseX_start)*floor((Time_Sim-Statin_doseX_start)/Statin_doseX_interval)*Statin_doseX_interval),lt(Time_Sim,Statin_doseX_start+Statin_doseX_duration+ge(Time_Sim,Statin_doseX_start)*floor((Time_Sim-Statin_doseX_start)/Statin_doseX_interval)*Statin_doseX_interval)),0)'
 
 def convert_long_fx_to_var(s):
-    """ converts the long piecewise functions to another variable """
+    """ converts the long piecewise functions to 0"""
     if s == p_1:
-        return 'f'
+        return 0
     elif s == p_2:
-        return 'g'
+        return 0
+    elif type(s) != str:
+        return s
     else:
+        for var in init_params.index:
+            if var in s:
+                s = s.replace(var, str(init_params.loc[var]))
+                print(str(init_params.loc[var]))
         return s
 
 # the piecewise functions cause errors when changing them to symbolic variables in python
-# will convery them to something else
+# will convert them to something else
+
+
+
+
 
 S_converted = S.applymap(convert_long_fx_to_var)
-
-
 
 S_sym = Matrix(S_converted.applymap(convert).values)
 
 null = S_sym.nullspace()
+
+print(len(null))
+
 
 
 m = []
@@ -40,6 +50,9 @@ for each in null:
     new_list = [i[0] for i in each.tolist()]
     m.append(new_list)
 pd.DataFrame(m).T.to_csv('../text_files/nullspace.csv')
+
+
+print(pd.DataFrame(m).T)
 
 null_sum = null[0]
 for i in range(1, len(null)):
