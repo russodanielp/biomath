a11ae2a3915f0c67b58c5f64411b8bec6286f631
Daniel P. Russo <russodanielp@gmail.com>
2018-04-28 21:21:18 -0400

    updated S matrix script to use fluxes generated by zhem

--- PeripheralModelFiles/python_scripts/make_s_matrix_from_odes.py
+++ PeripheralModelFiles/python_scripts/make_s_matrix_from_odes.py
@@ -5,6 +5,8 @@ odes = pd.read_csv('../text_files/odes.csv', header=None)
 
 reaction_fluxes = pd.read_csv('../text_files/reaction_fluxes.csv', header=None, sep='\t')
 
+zhems_fluxes = pd.read_csv('../text_files/zhems_flux.csv', header=None, sep='\t')
+
 def clean_up_equations(equation):
     """ cleans up the right hand portion of the text from an ODE"""
     # first remove unnecessary characters
@@ -40,39 +42,6 @@ def clean_up_reaction_fluxes(rxn_flx):
     return flx, S_component
 
 
-fluxes = []
-S_components = []
-
-for rxn_flx in reaction_fluxes[1]:
-    flx, S_component = clean_up_reaction_fluxes(rxn_flx)
-    fluxes.append(flx)
-    S_components.append(S_component)
-
-
-# create a dictionary to map the components that will go into the S matrix
-# with the names ReactionFlux, as listed in the ODEs
-
-S_component_map = dict(zip(['ReactionFlux{}'.format(i) for i in range(1, len(S_components)+1)], S_components))
-
-
-S_matrix = pd.DataFrame(index=odes[0].values, columns=['ReactionFlux{}'.format(i) for i in range(1, len(S_components)+1)])
-
-
-for idx, ode in odes.iterrows():
-    metabolite = ode[0]
-    right_hand_side = clean_up_equations(ode[1])
-    for rf in right_hand_side:
-
-        if rf[0] == '-':
-
-            S_matrix.loc[metabolite, rf.replace('-', '')] = '-' + S_component_map[rf.replace('-', '')]
-        else:
-
-            S_matrix.loc[metabolite, rf] = S_component_map[rf]
-
-S_matrix.fillna(0, inplace=True)
-
-S_matrix.to_csv('../text_files/S_matrix_python_27x60.csv')
 
 # there are two 'weird' ReactionFluxs, 45 and 46.
 # these are in the ODEs for VLDL_pl and Chol_ic_P
@@ -86,11 +55,13 @@ S_matrix.to_csv('../text_files/S_matrix_python_27x60.csv')
 # VLDL_pl_P_LDLRuptake_rate_k*LDLR_internalization_fraction_P*VLDL_pl*LDLR_cs_P
 # VLDL_pl_P_LDLRuptake_rate_k*LDLR_internalization_fraction_P*VLDL_pl*LDLR_cs_P*chol_per_VLDL_particle
 
+
 reaction_fluxes.loc[45, 1] = 'VLDL_pl_P_uptake_rate_k*VLDL_pl'
 reaction_fluxes.loc[46, 1] = 'VLDL_pl_P_uptake_rate_k*chol_per_VLDL_particle'
 
 reaction_fluxes.loc[60] = ['ReactionFlux61', 'VLDL_pl_P_LDLRuptake_rate_k*LDLR_internalization_fraction_P*VLDL_pl*LDLR_cs_P']
 reaction_fluxes.loc[61] = ['ReactionFlux62', 'VLDL_pl_P_LDLRuptake_rate_k*LDLR_internalization_fraction_P*VLDL_pl*LDLR_cs_P*chol_per_VLDL_particle']
+
 
 # need to replect this in the ODEs
 
@@ -98,21 +69,29 @@ reaction_fluxes.loc[61] = ['ReactionFlux62', 'VLDL_pl_P_LDLRuptake_rate_k*LDLR_i
 odes.loc[6, 1] = odes.loc[6, 1].replace('- ReactionFlux45', '- ReactionFlux45 - ReactionFlux61')
 odes.loc[9, 1] = odes.loc[9, 1].replace('+ ReactionFlux46', '+ ReactionFlux46 + ReactionFlux62')
 
-# now make new matrix
-
-fluxes = []
-S_components = []
-
-for rxn_flx in reaction_fluxes[1]:
-    flx, S_component = clean_up_reaction_fluxes(rxn_flx)
-    fluxes.append(flx)
-    S_components.append(S_component)
-
-
 # create a dictionary to map the components that will go into the S matrix
 # with the names ReactionFlux, as listed in the ODEs
 
-S_component_map = dict(zip(['ReactionFlux{}'.format(i) for i in range(1, len(S_components)+1)], S_components))
+S_component_map = {}
+i = 1
+for x, y in zip(reaction_fluxes[1].values, zhems_fluxes[1].values):
+    f_terms = y.strip().replace(';', '').split("*")
+
+    # there are some unique fluxes is just easier to manually fix
+    # such as 44
+    if f_terms == ['Chol_ic_P_to_HDL_pl_rate_k/chol_per_HDL_particle']:
+        S_components = ['Chol_ic_P', 'power(ApoA1_pl,ApoA1_RCT_nh)/(power(ApoA1_RCT_EC50,ApoA1_RCT_nh)+power(ApoA1_pl,ApoA1_RCT_nh))']
+    else:
+        rxn_terms = x.strip().replace(';', '').split("*")
+        S_components = [term for term in rxn_terms if term not in f_terms]
+        if not S_components:
+            S_components = ['1']
+
+    S_component_map['ReactionFlux{}'.format(i)] = "*".join(S_components)
+    i += 1
+    # print("=====")
+    # print("*".join(f_terms))
+    # print("*".join(rxn_terms))
 
 
 S_matrix = pd.DataFrame(index=odes[0].values, columns=['ReactionFlux{}'.format(i) for i in range(1, len(S_components)+1)])
